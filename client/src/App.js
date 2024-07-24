98b39b989b89287aa23516ad4e5deda12eb64892
neetesh566 <shakyaneetesh2@gmail.com>
2024-07-24 19:51:23 +0530

    package

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import './App.css';
+import Homepage from "./pages/Homepage";
+import Registerpage from"./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <div >
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="/register" element={<Registerpage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+
+      </Routes>
+      
+      </BrowserRouter>
       
     </div>
   );
