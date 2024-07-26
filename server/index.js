810c5a3d8fa9eb357f104f080bf0eb3db51e0319
neetesh566 <shakyaneetesh2@gmail.com>
2024-07-26 19:16:14 +0530

    Registation Page With Auth

--- server/index.js
+++ server/index.js
@@ -4,16 +4,20 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 
+const authRoutes = require("./routes/auth.js")
+
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+
+app.use("/auth",authRoutes)
 /*MONGOOSE SETUP*/
 const PORT = 3001;
 mongoose
   .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    dbName : "Holiday_Haven"
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
---
+++ server/routes/auth.js
@@ -0,0 +1,71 @@
+const router = require("express").Router();
+const bcyrpt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+
+const User = require("../models/User");
+const bcrypt = require("bcryptjs/dist/bcrypt");
+
+// configuration multer for file upload
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+/*USER REGISTER*/
+router.post("/register", upload.single("profileImage"), async (req, res) => {
+  try {
+    // Take all information from the form
+    const { firstName, lastName, email, password } = req.body;
+
+    // The uploaded file is available as req.file
+    const profileImage = req.file;
+
+    if (!profileImage) {
+      return res.status(400).send("No file uploaded");
+    }
+
+    // path to the uploaded profile photo
+    const ProfileImagePath = profileImage.path;
+
+    //check if user exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: "User already exists!" });
+    }
+    //Hass the password
+    const salt = await bcrypt.genSalt();
+    const hashedpassword = await bcrypt.hash(password, salt);
+
+    //create a new user
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedpassword,
+      ProfileImagePath,
+    });
+
+    //save the new user
+    await newUser.save();
+
+    //send a successful message
+    res
+      .status(200)
+      .json({ message: "User registered successfully!", user: newUser });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ message: "Registration failed!", error: err.message });
+  }
+});
+
+module.exports = router;
