6c8f66f9091895512064484325ba76f90fff67db
neetesh566 <shakyaneetesh2@gmail.com>
2024-07-28 13:03:30 +0530

    create listing page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Homepage from "./pages/Homepage";
 import Registerpage from"./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
+import CreateListing from "./pages/CreateListing";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Homepage/>}/>
         <Route path="/register" element={<Registerpage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/create-listing" element={<CreateListing/>}/>
 
       </Routes>
       
